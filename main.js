a3320caddd625c6b9119f35adad30e11633219a4
Ali Linares <alinares@raic.org>
2026-03-08 10:07:37 -0400

    Refactor chuck norris api search query

--- main.js
+++ main.js
@@ -12,14 +12,20 @@ window.addEventListener('load', () => {
 
 const jokeCallback = async (joke) => {
 	try{
-		let randomItemNumber = Math.floor(Math.random() * 51) + 1;
+		let randomItemNumber = Math.floor(Math.random() * 50);
 		let randomPageNumber = Math.floor(Math.random() * 5) + 1;;
 		let apiKey = '7J1JN67nA7jd2C2WEu5pd2ju2muVQhCW3O8Ikzs8gmagVbq0K0YiJ20wM8JT3REu';
 
-		const klipyResponse = await fetch(`https://api.klipy.com/api/v1/${apiKey}/gifs/search?page=${randomPageNumber}&per_page=50&q=chucknorris&customer_id=1`);
+		const klipyResponse = await fetch(`https://api.klipy.com/api/v1/${apiKey}/gifs/search?page=${randomPageNumber}&per_page=50&q=chuck%20norris&format_filter=gif&customer_id=1`);
 		const klipyData = await klipyResponse.json();
 
-		chuckImage.src = klipyData.data.data[randomItemNumber].file.sm.gif.url;
+		let klipyGifData = klipyData.data.data[randomItemNumber];
+		while (!/chuck\s+norris/i.test(klipyGifData.title)) {
+			randomItemNumber = Math.floor(Math.random() * klipyData.data.data.length);
+			klipyGifData = klipyData.data.data[randomItemNumber];
+		}
+		console.log(klipyGifData.title)
+		chuckImage.src = klipyGifData.file.sm.gif.url;
 		chuckImage.style.display = "block";
 
 		const response = await fetch('https://api.chucknorris.io/jokes/random');
